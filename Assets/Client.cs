0da27b9eda56f01fe2f2a094f9e4a56d27414a74
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-04-27 16:42:52 +0200

    feat(Client.cs, UIManager.cs, ServerScene.unity): working client in unity taht can connect to the server

--- Assets/Client.cs
+++ Assets/Client.cs
@@ -8,7 +8,7 @@ using System;
 
 public class Client : MonoBehaviour
 {
-    public static Client instance;
+    public static Client instance;  // instance
     public static int dataBufferSize = 4096;
 
     public string ip = "127.0.0.1";
@@ -16,13 +16,13 @@ public class Client : MonoBehaviour
     public int myId = 0;
     public TCP tcp;
 
-    private void Awake()
+    private void Awake()   // à l'instenciation
     {
-        if (instance == null)
+        if (instance == null)   // si y'a pas d'instance dit que celle ci est l'instance
         {
             instance = this;
         }
-        else if (instance != this)
+        else if (instance != this)  // sinon détruit l'instance comme ça y'a jamais 2 instances en même temps
         {
             Debug.Log("instance already exists, destroying object !");
             Destroy(this);
@@ -36,7 +36,7 @@ public class Client : MonoBehaviour
 
     public void ConnectToServer()
     {
-
+        tcp.Connect();
     }
 
     public class TCP
@@ -50,8 +50,8 @@ public class Client : MonoBehaviour
         {
             socket = new TcpClient
             {
-                ReceiveBufferSize = dataBufferSize,
-                SendBufferSize = dataBufferSize
+                ReceiveBufferSize = dataBufferSize, // donne a taille du buffer
+                SendBufferSize = dataBufferSize     // donne a taille du buffer
             };
 
             receiveBuffer = new byte[dataBufferSize];
@@ -69,7 +69,33 @@ public class Client : MonoBehaviour
 
             stream = socket.GetStream();
 
-            // stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLenght = stream.EndRead(_result); // Attend que la requête asynchrone en attente se termine
+                if ( _byteLenght <= 0) // si "_byteLenght" est vide : disconnect
+                {
+                    // TODO : disconnect
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLenght];
+                Array.Copy(receiveBuffer, _data, _byteLenght);  // met les infos reçus dans "_data"
+
+                // TODO : handle data
+
+                // Débute une opération de lecture asynchrone et apelle "ReceiveCallback" quand l'opération est terminée ( recommence quoi )
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+            }
+            catch
+            {
+                // TODO: disconnect
+            }
         }
     }
 
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager instance;
+
+    public GameObject startMenu;
+    public InputField usernameField;
+
+
+    private void Awake()   // à l'instenciation
     {
-        
+        if (instance == null)   // si y'a pas d'instance dit que celle ci est l'instance
+        {
+            instance = this;
+        }
+        else if (instance != this)  // sinon détruit l'instance comme ça y'a jamais 2 instances en même temps
+        {
+            Debug.Log("instance already exists, destroying object !");
+            Destroy(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ConnectToServer()
     {
-        
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
     }
 }
